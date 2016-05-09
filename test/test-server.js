3e51ebf120bcef7500850eb4d4a64253f588bbd8
Jim Strother <jim.strother@gmail.com>
2016-05-08 17:20:22 -0700

    working on new tests

---
+++ test/test-server.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+global.environment = 'test';
+var server = require('../server.js');
+var Item = require('../models/item');
+var seed = require('../db/seed');
+
+var should = chai.should();
+var app = server.app;
