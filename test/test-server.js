b8b9f084642f06c90b838dc178d601d8b6e62649
Jim Strother <jim.strother@gmail.com>
2016-05-09 18:12:47 -0700

    removed storage references from tests

--- test/test-server.js
+++ test/test-server.js
@@ -32,11 +32,11 @@ describe('Shopping List', function() {
                 res.body.should.be.a('array');
                 res.body.should.have.length(3);
                 res.body[0].should.be.a('object');
-                res.body[0].should.have.property('id');
+                res.body[0].should.have.property('_id');
                 res.body[0].should.have.property('name');
-                res.body[0].id.should.be.a('string');
+                res.body[0]._id.should.be.a('string');
                 res.body[0].name.should.be.a('string');
-                res.body[0].name.should.equal('Broad beans');
+                res.body[0].name.should.equal('Broad Beans');
                 res.body[1].name.should.equal('Tomatoes');
                 res.body[2].name.should.equal('Peppers');
                 done();
@@ -52,38 +52,28 @@ describe('Shopping List', function() {
                 res.should.be.json;
                 res.body.should.be.a('object');
                 res.body.should.have.property('name');
-                res.body.should.have.property('id');
+                res.body.should.have.property('_id');
                 res.body.name.should.be.a('string');
-                res.body.id.should.be.a('string');
+                res.body._id.should.be.a('string');
                 res.body.name.should.equal('Kale');
-                storage.items.should.be.a('array');
-                storage.items.should.have.length(4);
-                storage.items[3].should.be.a('object');
-                storage.items[3].should.have.property('id');
-                storage.items[3].should.have.property('name');
-                storage.items[3].id.should.be.a('number');
-                storage.items[3].name.should.be.a('string');
-                storage.items[3].name.should.equal('Kale');
                 done();
             });
     });
     it('should edit an item on PUT', function(done) {
         chai.request(app)
             .put('/items/0')
-            .send({'name': 'Black beans'})
+            .send({'name': 'Black Beans'})
             .end(function(err, res) {
                 should.equal(err, null);
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('object');
                 res.body.should.have.property('name');
-                res.body.should.have.property('id');
+                res.body.should.have.property('_id');
                 res.body.name.should.be.a('string');
-                res.body.id.should.be.a('string');
-                res.body.id.should.equal(0);
-                res.body.name.should.equal('Black beans');
-                storage.items.should.be.a('array');
-                storage.items[0].name.should.equal('Black beans');
+                res.body._id.should.be.a('string');
+                res.body._id.should.equal(0);
+                res.body.name.should.equal('Black Beans');
                 done();
             });
     });
@@ -97,13 +87,11 @@ describe('Shopping List', function() {
             res.should.be.json;
             res.body.should.be.a('object');
             res.body.should.have.property('name');
-            res.body.should.have.property('id');
+            res.body.should.have.property('_id');
             res.body.name.should.be.a('string');
-            res.body.id.should.be.a('string');
-            res.body.id.should.equal(4);
+            res.body._id.should.be.a('string');
+            res.body._id.should.equal(4);
             res.body.name.should.equal('Green Beans');
-            storage.items.should.be.a('array');
-            storage.items[4].name.should.equal('Green Beans');
             done();
         });
     });
@@ -115,8 +103,6 @@ describe('Shopping List', function() {
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('object');
-                storage.items.should.be.a('array');
-                storage.items[0].name.should.not.equal('Black beans');
                 done();
             });
     });
@@ -135,4 +121,3 @@ describe('Shopping List', function() {
 });
 
 exports.app = app;
-// exports.storage = storage;
